1aa57c36aeb00b61ecba4e230598b8a10ff143c0
Christopher Sannar <chris.sannar.dev@gmail.com>
2020-03-03 20:42:05 -0700

    Init

---
+++ src/main/java/com/example/httpsandboxbackend/HttpSandboxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.httpsandboxbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpSandboxBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpSandboxBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/httpsandboxbackend/HttpSandboxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.httpsandboxbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpSandboxBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
